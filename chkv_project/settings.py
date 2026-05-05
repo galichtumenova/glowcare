93ce5796564002eab8f628a1152ac4a3ef496f53
galiya <galiyatumenova@icloud.com>
2026-05-05 08:13:10 +0600

    fix postgres database

--- chkv_project/settings.py
+++ chkv_project/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'chkv_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=not DEBUG
     )
 }
 
